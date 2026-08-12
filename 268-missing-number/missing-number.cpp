b77d32bf400eecbd7497afbdd0ac634fd5908ad8
sathvikr333 <sathvikr333@gmail.com>
2026-08-13 00:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (94.44%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& arr) {
-        int n = arr.size() + 1;
-        vector <int> hash(n+1,0);
-        for(int i = 0; i < n - 1;i++) {
-            hash[arr[i]] = 1;
-
+        int n = arr.size();
+        int sum = ((n *(n + 1))/2);
+        int s2 = 0;
+        for(int i = 0; i < n;i++) {
+            s2 += arr[i];
         }
-        for(int i = 0; i <= n;i++) {
-            if(hash[i] == 0) {
-                return i;
-            }
-        }
-        return -1;
+        return sum - s2;
     }
 };
