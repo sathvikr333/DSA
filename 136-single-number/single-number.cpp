3a5d197b48b612693e60e889692ee211548a7f28
sathvikr333 <sathvikr333@gmail.com>
2026-08-14 00:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (98.96%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i = 0; i < nums.size();i++) {
-            int count = 0;
-            for(int j = 0; j < nums.size();j++) {
-                if(nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if(count == 1) {
-                return nums[i];
-            }
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0;i < n;i++) {
+            ans = ans ^ nums[i];
         }
-        return -1;
+        return ans;
     }
 };
