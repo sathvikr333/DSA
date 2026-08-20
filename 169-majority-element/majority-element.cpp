c6354e976a80adc79ca49805e34f22d01821c5d7
sathvikr333 <sathvikr333@gmail.com>
2026-08-20 23:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (75.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -16,15 +16,7 @@ public:
                 cnt--;            
             }
         }
-        int cnt1 = 0;
-        for(int i = 0;i < nums.size();i++) {
-           if(nums[i] == el) {
-            cnt1++;
-        }
-        }
-        if(cnt1 > nums.size()/2) {
-            return el;
-        }
-        return -1;
+        return el;
+        
     }
 };
