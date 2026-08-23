fb74ee8993659a3f1faaf6e3258ee1e1b08e2baf
sathvikr333 <sathvikr333@gmail.com>
2026-08-23 09:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.24%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,10 +3,10 @@ public:
     int missingNumber(vector<int>& arr) {
         int n = arr.size();
         int ans = n;
-       for(int i = 0;i < n;i++) {
-        ans = ans ^ i;
-        ans = ans ^ arr[i];
-       }
-       return ans;
+        for(int i = 0;i < n;i++) {
+            ans = ans ^ i;
+            ans = ans ^ arr[i];
+        }
+        return ans;
     }
 };
