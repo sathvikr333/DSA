968aabe263e6f3646296f45a57d728d904adf3b3
sathvikr333 <sathvikr333@gmail.com>
2026-09-02 17:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (95.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,46 +4,40 @@ public:
         int cnt1 = 0;
         int cnt2 = 0;
         int el1 = INT_MIN;
-        int el2 = INT_MIN;
+        int el2=INT_MIN;
         vector<int> c;
         for(int i = 0;i < nums.size();i++) {
-            if(cnt1 == 0 && nums[i] != el2) {
+            if(cnt1 == 0 && el2 != nums[i]) {
                 cnt1 = 1;
                 el1 = nums[i];
             }
-            else if(cnt2 == 0 && nums[i] != el1) {
-                cnt2 = 1;
-                el2 = nums[i];
+            else if (cnt2 == 0 && el1 != nums[i]) {
+                    cnt2 = 1;
+                    el2 = nums[i];
             }
-            else if (el1 == nums[i] ) {
+            else if(nums[i] == el1) {
                 cnt1++;
             }
-            else if(el2 == nums[i] ) {
+            else if(nums[i] == el2) {
                 cnt2++;
             }
             else {
                 cnt1--;
                 cnt2--;
-            }
+            } 
         }
         cnt1 = 0;
         cnt2 = 0;
         for(int i = 0;i < nums.size();i++) {
-            if(nums[i] == el1) {
-                cnt1++;
-                
-            }
-            if(nums[i] == el2) {
-                cnt2++;
-                
-            }
+            if(nums[i] == el1) cnt1++;
+            if(nums[i] == el2) cnt2++;
         }
         if(cnt1 > nums.size()/3) {
-                    c.push_back(el1);
-                }
+            c.push_back(el1);
+        }
         if(cnt2 > nums.size()/3) {
-                    c.push_back(el2);
-                }
+            c.push_back(el2);
+        }
         return c;
 
     }
