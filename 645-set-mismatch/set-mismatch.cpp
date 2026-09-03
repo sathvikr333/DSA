655143b932864a27fc9eeaa8c7d906a5b1518130
sathvikr333 <sathvikr333@gmail.com>
2026-09-03 21:10:27 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (74.08%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int duplicate;
-        int missing;
-        for(int i = 1;i <= nums.size();i++) {
-            int count = 0;
-            for(int x:nums) {
-                if(x == i) {
-                    count++;
-                }
-            }
-            if(count == 2) {
-              duplicate = i;
-            }
-            else if(count == 0) {
-                missing = i;
-            }
+        long long n = nums.size();
+        long long SN = (n*(n+1))/2;
+        long long S2N =(n * (n + 1)* (2*n+1))/6;
+        long long S = 0;
+        long long S2 = 0;
+        for(int i = 0;i < nums.size();i++) {
+            S += nums[i];
+            S2 += (long long)nums[i] * (long long)nums[i];
         }
-        return {duplicate,missing};
+        long long val_1 = S - SN;
+        long long val_2 = S2 - S2N;
+        val_2 = val_2/val_1;
+        long long  x = (val_1 + val_2)/2;
+        long long  y = x - val_1;
+        return{(int)x,(int)y};
+
            
     }
 };
